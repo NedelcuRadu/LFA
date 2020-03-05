584e5dee45c229f7fc809073982e403259759859
Nedelcu Radu <43296487+NedelcuRadu@users.noreply.github.com>
2020-03-05 11:25:18 +0200

    Create Main.py

---
+++ DFA_Accepter/Main.py
@@ -0,0 +1,38 @@
+
+def eval(cuvant):
+    global A, final, Q
+    stare_curenta = Q
+    print(cuvant)
+    while (len(cuvant)) > 0:
+        stare_curenta = A[stare_curenta][cuvant[0]]
+        if stare_curenta == -1:
+            return False
+        cuvant = cuvant[1:]
+    if stare_curenta in final:
+        return True
+    else:
+        return False
+
+
+with open("data.in") as f:
+    A = []
+    n = int(f.readline())
+    m = int(f.readline())
+    alfabet = [x for x in f.readline().split()]
+    Q = int(f.readline())
+    k = int(f.readline())
+    final = [int(x) for x in f.readline().split()]
+    i = int(f.readline())
+    for i in range(n):
+        temp = dict()
+        for i in alfabet:
+            temp[i] = -1
+        A.append(temp)
+    for linie in f:
+        translatie = [x for x in linie.split()]
+        A[int(translatie[0])][translatie[1]] = int(translatie[2])
+    print(A)
+
+with open("cuvinte.in") as f:
+    for linie in f:
+        print(eval(linie.rstrip()))
