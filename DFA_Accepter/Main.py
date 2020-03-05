2460d34d9e57f7b07e46cc03188f8261ceb0393c
Nedelcu Radu <43296487+NedelcuRadu@users.noreply.github.com>
2020-03-05 11:29:09 +0200

    Update Main.py

--- DFA_Accepter/Main.py
+++ DFA_Accepter/Main.py
@@ -1,3 +1,16 @@
+"""
+DFA ACCEPTER - Checkes whether or not a word is processed by a given DFA
+
+Data Format: n - number of states
+            m - size of alphabet
+            - Line of m alphabet chars -
+            Initial state
+            k - number of final states
+            - Line of k final states -
+            l - number of translations
+            l lines of x a b ( from state x with translation a you go to state b)
+
+"""
 
 def eval(cuvant):
     global A, final, Q
